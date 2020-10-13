164d8d02f457c7fce0931065ca4cfc6994c378e8
alucaproject <alucaproject@gmail.com>
2020-10-12 19:24:41 -0500

    Updates

---
+++ precache-manifest.85843dc9166548e237e1da4044ad6755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7698e850c37c819840813084903faa56",
+    "url": "/calculatorReactJs/index.html"
+  },
+  {
+    "revision": "01e4bf4d29cef3b5f39e",
+    "url": "/calculatorReactJs/static/css/main.c78662a3.chunk.css"
+  },
+  {
+    "revision": "af079e12558c08281044",
+    "url": "/calculatorReactJs/static/js/2.ba223bb1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculatorReactJs/static/js/2.ba223bb1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01e4bf4d29cef3b5f39e",
+    "url": "/calculatorReactJs/static/js/main.76d08636.chunk.js"
+  },
+  {
+    "revision": "02341224a5c228f7589a",
+    "url": "/calculatorReactJs/static/js/runtime-main.2dfbcbef.js"
+  }
+]);
